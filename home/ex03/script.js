c778db455b1e824515f4c0fc1ec12017030a96a8
Djalma Henrique <djalmahenry@outlook.com>
2022-04-09 14:09:40 -0300

    feat: add counter instantiator

--- home/ex03/script.js
+++ home/ex03/script.js
@@ -1,18 +1,53 @@
-let counter = {
-    one: document.getElementById('counter-one'),
-    two: document.getElementById('counter-two'),
+let id = 0;
+let counter = [];
+
+function createCounter() {
+  let id = getNextId(); // pega o id mais alto
+
+  const divCounters = document.getElementById("counters"); // pega o div que contém os contadores
+
+  const text = document.getElementById("counter-name").value; // pega o valor do input
+  document.getElementById("counter-name").value = ""; // limpa o input
+
+  const elementDiv = document.createElement("div"); // cria um novo div
+
+  elementDiv.id = id; // atribui o id
+  elementDiv.className = "counter"; // atribui a classe
+
+  const p = document.createElement("p"); // cria um novo p para o nome
+  const p2 = document.createElement("p"); // cria um novo p para o contador
+  const button = document.createElement("button"); // cria um novo button para zerar o contador
+
+  p2.id = id; // atribui o id
+
+  p.innerHTML = text; // atribui o texto do input ao p
+  button.innerHTML = "Reset"; // atribui o texto do button ao button
+  counter.push(0); // adiciona o contador ao array
+  p2.innerHTML = counter[id];  // atribui o valor do contador ao p2
+
+  elementDiv.onclick = function () { // atribui o evento de click de incrementar contador
+    incrementCounter(p2);
+  }; 
+  button.onclick = function () { // atribui o evento de click de resetar contador
+    reset(p2);
+  };
+
+  elementDiv.appendChild(p); //Adiciona o nome a div
+  elementDiv.appendChild(p2); //Adiciona o contador a div
+  elementDiv.appendChild(button); //Adiciona o botao de reset a div
+  elementDiv.style.backgroundColor = "#f0" + id.toString() * 2; // atribui a cor de fundo ao div do contador
+  divCounters.appendChild(elementDiv); //Adiciona a div a div de contadores
 }
 
-counter.one.value = '0';
-counter.two.value = '0';
-
-function clickDiv(counter) {
-    let value = parseInt(counter.value) + 1;
-    counter.innerHTML = value;
-    counter.value = value;
+function getNextId() {
+  return id++;
 }
 
-function reset(counter) {
-    counter.innerHTML = -1;
-    counter.value = -1;
+function incrementCounter(e) {
+  e.innerHTML = ++counter[e.id]; // incrementa o contador
+}
+
+function reset(e) { // reseta o contador
+  counter[e.id] = -1;
+  e.innerHTML = counter[e.id];
 }
