cbe717c5a74c0e1f87ec0e2b3bb5ffde8fba61a9
Djalma Henrique <djalmahenry@outlook.com>
2022-04-21 11:47:49 -0300

    feat: add button to more projects

--- home/ex04-my-portfolio/script.js
+++ home/ex04-my-portfolio/script.js
@@ -57,7 +57,7 @@ const scrollReveal = ScrollReveal({
 scrollReveal.reveal(`
 #home .image, #home .text,
 #about .image, #about .text,
-#projects header, #projects .card,
+#projects header, #projects .card, #projects .projects-button,
 #testimonials header, #testimonials .testimonials,
 #contact .text, #contact .links,
 footer .brand, footer .social
